fcde98fbb2a6b97f6d6218e109ff347d2f6a89aa
santosh-ingawale <santosh@headfitted.com>
2013-03-15 21:28:09 -0700

    Employee

---
+++ EmployeeMgmtSilverlightApplication/EmployeeMgmtSilverlightApplication.Web/EmployeeService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using EmployeeEntity;
+using EmployeeBusinesslayer;
+using System.Collections.Generic;
+
+namespace EmployeeMgmtSilverlightApplication.Web
+{
+    [ServiceContract(Namespace = "")]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class EmployeeService
+    {
+        Entity ent = new Entity();
+        [OperationContract]
+        public void DoWork(Entity ent)
+        {
+            // Add your operation implementation here
+            return;
+        }
+        [OperationContract]
+        public int Employee(Entity ent)
+        {
+
+          
+            Business b = new Business();
+            return b.CreateEmp(ent);
+
+        }
+
+        [OperationContract]
+        public List<Entity> SelectAll()
+        {
+           
+            Business bb = new Business();
+            return bb.SelectEmployee();
+
+
+            //return getemp.ToList();
+        }
+        // Add more operations here and mark them with [OperationContract]
+    }
+}
